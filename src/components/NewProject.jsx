87b7acd78ad8b275e4d43951e1c7c19bd8abd9e0
guilherme1s <2000guilherme.silva@gmail.com>
2025-02-10 10:14:15 -0300

    feat: add pojects sidebar component

---
+++ src/components/NewProject.jsx
@@ -0,0 +1,19 @@
+import Input from "./Input";
+
+export default function NewProject() {
+    return(
+        <div className="w-[35rem] mt-16">
+            <menu className="flex items-center justify-end gap-4 my-4">
+                <li>
+                    <button className="text-stone-800 hover:text-stone-950">Cancelar</button></li>
+                <li>
+                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Salvar</button></li>
+            </menu>
+            <div>
+                <Input label="Título" />
+                <Input label="Descrição" textarea />
+                <Input label="Prazo" /> 
+            </div>
+        </div>
+    );
+}
