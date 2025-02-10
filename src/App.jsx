025f66bc21268339bf97ce23ace7bd70974b1c80
guilherme1s <2000guilherme.silva@gmail.com>
2025-02-10 12:09:23 -0300

    feat: add projects to list

--- src/App.jsx
+++ src/App.jsx
@@ -7,14 +7,30 @@ import NoProjectSelected from "./components/noProjectSelected";
 export function App() {
   const [projectsState, setProjectsState] = useState({
     selectedProjectId: undefined,
-    projects: []
+    projects: [],
   });
 
   function handleStartAddProject() {
-    setProjectsState(prevState => {
+    setProjectsState((prevState) => {
       return {
         ...prevState,
-        selectedProjectId: null
+        selectedProjectId: null,
+      };
+    });
+  }
+
+  function HandleAddProject(projectData) {
+    setProjectsState((prevState) => {
+      const projectId = Math.random();
+      const newProject = {
+        ...projectData,
+        id: projectId,
+      };
+
+      return {
+        ...prevState,
+        selectedProjectId: undefined,
+        projects: [...prevState.projects, newProject],
       };
     });
   }
@@ -22,14 +38,17 @@ export function App() {
   let content;
 
   if (projectsState.selectedProjectId === null) {
-    content = <NewProject />
+    content = <NewProject onAdd={HandleAddProject} />;
   } else if (projectsState.selectedProjectId === undefined) {
-    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
   }
 
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectsSidebar onStartAddProject={handleStartAddProject} />
+      <ProjectsSidebar
+        onStartAddProject={handleStartAddProject}
+        projects={projectsState.projects}
+      />
       {content}
     </main>
   );
--- src/components/NewProject.jsx
+++ src/components/NewProject.jsx
@@ -1,19 +1,46 @@
+import { useRef } from "react";
+
 import Input from "./Input";
 
-export default function NewProject() {
-    return(
-        <div className="w-[35rem] mt-16">
-            <menu className="flex items-center justify-end gap-4 my-4">
-                <li>
-                    <button className="text-stone-800 hover:text-stone-950">Cancelar</button></li>
-                <li>
-                    <button className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950">Salvar</button></li>
-            </menu>
-            <div>
-                <Input label="Título" />
-                <Input label="Descrição" textarea />
-                <Input label="Prazo" /> 
-            </div>
-        </div>
-    );
+export default function NewProject({ onAdd }) {
+  const title = useRef();
+  const description = useRef();
+  const dueDate = useRef();
+
+  function handleSave() {
+    const enteredTitle = title.current.value;
+    const enteredDescription = description.current.value;
+    const enteredDueDate = dueDate.current.value;
+
+    onAdd({
+      title: enteredTitle,
+      description: enteredDescription,
+      dueDate: enteredDueDate,
+    });
+  }
+
+  return (
+    <div className="w-[35rem] mt-16">
+      <menu className="flex items-center justify-end gap-4 my-4">
+        <li>
+          <button className="text-stone-800 hover:text-stone-950">
+            Cancelar
+          </button>
+        </li>
+        <li>
+          <button
+            onClick={handleSave}
+            className="px-6 py-2 rounded-md bg-stone-800 text-stone-50 hover:bg-stone-950"
+          >
+            Salvar
+          </button>
+        </li>
+      </menu>
+      <div>
+        <Input type="text" ref={title} label="Título" />
+        <Input ref={description} label="Descrição" textarea />
+        <Input type="date" ref={dueDate} label="Prazo" />
+      </div>
+    </div>
+  );
 }
